3cdbf1533372f7ecf0d30bf0f903a9ad1aa579d4
Marvin Ramos <mr487223@gmail.com>
2024-04-24 15:49:01 -0600

    Refactor materials and renderer in app.js for chapter-01/02-first-scene

--- chapter-01/02-first-scene/app.js
+++ chapter-01/02-first-scene/app.js
@@ -1,54 +1,53 @@
 import * as THREE from './node_modules/three/build/three.module.js';
 import { TrackballControls } from './node_modules/three/examples/jsm/controls/TrackballControls.js';
 
-// creating scene
+// Creando la escena
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 
 renderer.setClearColor(0xEEEEEE);
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true; // Cambiado a shadowMap.enabled
 
 document.body.appendChild(renderer.domElement);
 
-
-// creating axes helper
+// Creando el helper de ejes
 const axes = new THREE.AxesHelper(20);
 scene.add(axes);
 
-// creating plane ground
+// Creando el plano
 const planeGeometry = new THREE.PlaneGeometry(60, 20);
 const planeMaterial = new THREE.MeshLambertMaterial({ color: 0xcccccc });
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 
-// rotate and position the plane
 plane.rotation.x = -0.5 * Math.PI;
 plane.position.x = 15;
 plane.position.y = 0;
 plane.position.z = 0;
 
+plane.receiveShadow = true;
 scene.add(plane);
 
+// Cubo
 const cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
-const cubeMaterial = new THREE.MeshLambertMaterial({ color: 0xff0000});
+const cubeMaterial = new THREE.MeshLambertMaterial({ color: 0xff0000 });
 const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
 cube.position.x = -4;
 cube.position.y = 3;
 cube.position.z = 0;
 
+cube.castShadow = true; // Habilitar sombra
 scene.add(cube);
 
-
+// Esfera
 const sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
 const sphereMaterial = new THREE.MeshLambertMaterial({ color: 0x7777ff });
 const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 
-// sphere.position.x = 20;
-// sphere.position.y = 4;
-// sphere.position.z = 2;
 sphere.position.set(20, 4, 2);
-
+sphere.castShadow = true;
 scene.add(sphere);
 
 camera.position.x = -30;
@@ -56,13 +55,14 @@ camera.position.y = 40;
 camera.position.z = 30;
 camera.lookAt(scene.position);
 
-// adding spotlights
+// Luz puntual
 const spotLight = new THREE.SpotLight(0xffffff);
 spotLight.position.set(-40, 60, -10);
+spotLight.castShadow = true;
 scene.add(spotLight);
 
-// Agregar luz ambiental
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); 
+// Luz ambiental
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
 scene.add(ambientLight);
 
 const controls = new TrackballControls(camera, renderer.domElement);
