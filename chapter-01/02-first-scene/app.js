dd22f4a2558c56ad744cc43dcf29b6260824ed74
Marvin Ramos <mr487223@gmail.com>
2024-04-24 15:35:47 -0600

    Refactor materials and renderer in app.js for chapter-01/02-first-scene

--- chapter-01/02-first-scene/app.js
+++ chapter-01/02-first-scene/app.js
@@ -18,7 +18,6 @@ scene.add(axes);
 
 // creating plane ground
 const planeGeometry = new THREE.PlaneGeometry(60, 20);
-// const planeMaterial = new THREE.MeshBasicMaterial({ color: 0xcccccc });
 const planeMaterial = new THREE.MeshLambertMaterial({ color: 0xcccccc });
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 
@@ -31,7 +30,6 @@ plane.position.z = 0;
 scene.add(plane);
 
 const cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
-// const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000});
 const cubeMaterial = new THREE.MeshLambertMaterial({ color: 0xff0000});
 const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
@@ -43,13 +41,13 @@ scene.add(cube);
 
 
 const sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
-// const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0x7777ff });
 const sphereMaterial = new THREE.MeshLambertMaterial({ color: 0x7777ff });
 const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 
-sphere.position.x = 20;
-sphere.position.y = 4;
-sphere.position.z = 2;
+// sphere.position.x = 20;
+// sphere.position.y = 4;
+// sphere.position.z = 2;
+sphere.position.set(20, 4, 2);
 
 scene.add(sphere);
 
@@ -63,12 +61,21 @@ const spotLight = new THREE.SpotLight(0xffffff);
 spotLight.position.set(-40, 60, -10);
 scene.add(spotLight);
 
+// Agregar luz ambiental
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); 
+scene.add(ambientLight);
+
 const controls = new TrackballControls(camera, renderer.domElement);
 controls.rotateSpeed = 4;
 controls.dynamicDampingFactor = 0.15;
 
-
-
+window.addEventListener('resize', function () {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    renderer.setSize(width, height);
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+});
 
 const render = function () {
     requestAnimationFrame(render);
