46c7964498b0e3a4c70eae636b8acf090385d1a0
Marvin Ramos <mr487223@gmail.com>
2024-04-26 10:56:35 -0600

    create a simple scene

---
+++ chapter-01/examples/02-first-scene.js
@@ -0,0 +1,57 @@
+// import * as THREE from '../three/build/three.module.js';
+import * as THREE from 'three';
+import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls.js';
+import * as dat from 'dat.gui';
+import Stats from 'stats.js';
+import { AsciiEffect } from 'three/examples/jsm/effects/AsciiEffect.js'; 
+
+function init(){
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setClearColor(0xEEEEEE);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    const axes = new THREE.AxesHelper(20);
+    scene.add(axes);
+
+    const planeGeometry = new THREE.PlaneGeometry(60, 20);
+    const planeMaterial = new THREE.MeshBasicMaterial({ color: 0xcccccc });
+    const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    plane.rotation.x = -0.5 * Math.PI;
+    plane.position.x = 15;
+    plane.position.y = 0;
+    plane.position.z = 0;
+    scene.add(plane);
+
+    const cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
+    const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+    const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    cube.position.x = -4;
+    cube.position.y = 3;
+    cube.position.z = 0;
+    cube.castShadow = true;
+    scene.add(cube);
+
+    const sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
+    const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0x7777ff });
+    const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    
+    sphere.position.set(20, 4, 2);
+    scene.castShadow = true;
+    scene.add(sphere);
+
+    // position and point the camera to the center of the scene
+    camera.position.x = -30;
+    camera.position.y = 40;
+    camera.position.z = 30;
+    camera.lookAt(scene.position);
+
+    document.body.appendChild(renderer.domElement);
+
+    renderer.render(scene, camera);
+
+}
+
+window.onload = init;
