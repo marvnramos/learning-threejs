793fa7c909f46cd87c3069a5c32e05eee5d4b836
Marvin Ramos <mr487223@gmail.com>
2024-04-29 09:36:37 -0600

    add lights and shadows

--- chapter-01/examples/03-materials-light.js
+++ chapter-01/examples/03-materials-light.js
@@ -1,30 +1,35 @@
 import * as THREE from 'three';
 import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls.js';
-import * as dat from 'dat.gui';
-import Stats from 'stats.js';
-import { AsciiEffect } from 'three/examples/jsm/effects/AsciiEffect.js'; 
 
 function init(){
     const scene = new THREE.Scene();
+
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.x = -30;
+    camera.position.y = 40;
+    camera.position.z = 30;
+    camera.lookAt(scene.position);
+
 
     const renderer = new THREE.WebGLRenderer();
-    renderer.setClearColor(0xEEEEEE);
     renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.shadowMap.enabled = true; // Habilitar el renderizado de sombras
-    renderer.shadowMap.type = THREE.PCFSoftShadowMap; // Tipo de sombras suaves
+    renderer.setClearColor(0x999999);
+    renderer.shadowMap.enabled = true;
+    
+
+    document.body.appendChild(renderer.domElement);
 
     const axes = new THREE.AxesHelper(20);
     scene.add(axes);
 
-    const planeGeometry = new THREE.PlaneGeometry(60, 20, 32, 32);
-    const planeMaterial = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
+    const planeGeometry = new THREE.PlaneGeometry(40, 20, 32, 32);
+    const planeMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff });
     const plane = new THREE.Mesh(planeGeometry, planeMaterial);
     plane.rotation.x = -0.5 * Math.PI;
-    plane.position.x = 15;
+    plane.position.x = 8;
     plane.position.y = 0;
     plane.position.z = 0;
-    plane.receiveShadow = true; // Permitir que el plano reciba sombras
+    plane.receiveShadow = true;
     scene.add(plane);
 
     const cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
@@ -37,42 +42,41 @@ function init(){
     scene.add(cube);
 
     const sphereGeometry = new THREE.SphereGeometry(4, 32, 32);
-    const sphereMaterial = new THREE.MeshStandardMaterial({ color: 0x7777ff });
+    const sphereMaterial = new THREE.MeshLambertMaterial({ color: 0x7777ff, wireframe: true});
     const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
     sphere.position.set(20, 4, 2);
-    sphere.receiveShadow = false; //default
     sphere.castShadow = true;
     scene.add(sphere);
 
-    // position and point the camera to the center of the scene
-    camera.position.x = -30;
-    camera.position.y = 40;
-    camera.position.z = 30;
-    camera.lookAt(scene.position);
-
-    // Agregar luz ambiental
-    const ambientLight = new THREE.AmbientLight(0x404040, 1); // Color de la luz ambiental
-    scene.add(ambientLight);
-
-    const light = new THREE.SpotLight( 0xffffff );
-    light.castShadow = true; // default false
-    scene.add( light );
     
-    //Set up shadow properties for the light
-    light.shadow.mapSize.width = 300; // default
-    light.shadow.mapSize.height = 512; // default
-    light.shadow.camera.near = 0.5; // default
-    light.shadow.camera.far = 500; // default
-    light.shadow.focus = 1; // default
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    directionalLight.position.set(0, 10, 5) ;
+    directionalLight.castShadow = true;
+    scene.add(directionalLight);
 
-    const helper = new THREE.CameraHelper( light.shadow.camera );
-scene.add( helper );
+    directionalLight.shadow.mapSize.width = window.innerWidth;
+    directionalLight.shadow.mapSize.height = window.innerHeight;
+    directionalLight.shadow.camera.top = 50;
+    directionalLight.shadow.camera.bottom = -50;
+    directionalLight.shadow.camera.left = -50;
+    directionalLight.shadow.camera.right = 50;
+    directionalLight.shadow.camera.near = 0.5;
+    directionalLight.shadow.camera.far = 200;
 
-    document.body.appendChild(renderer.domElement);
+    const DirectionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 5);
+    scene.add(DirectionalLightHelper);
 
     const controls = new TrackballControls(camera, renderer.domElement);
     controls.rotateSpeed = 4;
     controls.dynamicDampingFactor = 0.15;
+
+    window.addEventListener('resize', function () {
+        const width = window.innerWidth;
+        const height = window.innerHeight;
+        renderer.setSize(width, height);
+        camera.aspect = width / height;
+        camera.updateProjectionMatrix();
+    });
 
     function render(){
         requestAnimationFrame(render);
