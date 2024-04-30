216b24b1ffdc1c42557a8ede52309a281f0e14f4
Marvin Ramos <mr487223@gmail.com>
2024-04-30 09:45:02 -0600

    Refactor trackball controls and fix add geometries function

--- chapter-02/examples/04-geometries.js
+++ chapter-02/examples/04-geometries.js
@@ -1,6 +1,10 @@
 import * as THREE from 'three';
 import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls.js';
+import {ConvexGeometry} from 'three/examples/jsm/geometries/ConvexGeometry.js';
 import Stats from 'stats.js';
+import * as SceneUtils from 'three/addons/utils/SceneUtils.js';
+import { ParametricGeometry } from 'three/addons/geometries/ParametricGeometry.js';
+
 
 const init = () => {
     const scene = new THREE.Scene();
@@ -8,14 +12,14 @@ const init = () => {
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     scene.add(camera);
 
-    camera.position.x = -30;
-    camera.position.y = 40;
-    camera.position.z = 30;
-    camera.lookAt(scene.position);
+    camera.position.x = -50;
+    camera.position.y = 30;
+    camera.position.z = 20;
+    camera.lookAt(new THREE.Vector3(-10, 0, 0));
 
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setClearColor(0xEEEEEE);
+    renderer.setClearColor(new THREE.Color(0x000000));
     renderer.shadowMap.enabled = true;
 
     const planeGeometry = new THREE.PlaneGeometry(60, 40, 1, 1);
@@ -30,18 +34,96 @@ const init = () => {
 
     scene.add(plane);
 
-    const ambientLight = new THREE.AmbientLight(0xffffff);
+    const ambientLight = new THREE.AmbientLight(0x090909);
     scene.add(ambientLight);
 
     const directionalLight = new THREE.DirectionalLight(0xffffff);
-    directionalLight.position.set(-40, 60, -10);
+    directionalLight.position.set(-40, 40, 50);
     directionalLight.castShadow = true;
     scene.add(directionalLight);
 
     const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight);
     scene.add(directionalLightHelper);
 
+
+    let step = 0;
+
+        
+    const addGeometries = (scene) => {
+        const geoms = [];
+
+        geoms.push(new THREE.CylinderGeometry(1, 4, 4));
+        geoms.push(new THREE.BoxGeometry(2, 2, 2));
+        geoms.push(new THREE.SphereGeometry(2));
+        geoms.push(new THREE.IcosahedronGeometry(4));
+
+        const points = [
+            new THREE.Vector3(2, 2, 2),
+            new THREE.Vector3(2, 2, -2),
+            new THREE.Vector3(-2, 2, -2),
+            new THREE.Vector3(-2, 2, 2),
+            new THREE.Vector3(2, -2, 2),
+            new THREE.Vector3(2, -2, -2),
+            new THREE.Vector3(-2, -2, -2),
+            new THREE.Vector3(-2, -2, 2)
+        ];
+        geoms.push(new ConvexGeometry(points));
+
+        const pts = [];
+        const detail = 0.1;
+        const radius = 3;
+        for (let angle = 0.0; angle < Math.PI; angle += detail)
+            pts.push(new THREE.Vector3(Math.cos(angle) * radius, 0, Math.sin(angle) * radius));
+        geoms.push(new THREE.LatheGeometry(pts, 12));
+
+        geoms.push(new THREE.OctahedronGeometry(3));
+
+        geoms.push(new ParametricGeometry(ParametricGeometry.mobius3d, 20, 10));
+
+        geoms.push(new THREE.TetrahedronGeometry(3));
+
+        geoms.push(new THREE.TorusGeometry(3, 1, 10, 10));
+
+        geoms.push(new THREE.TorusKnotGeometry(3, 0.5, 50, 20));
+
+        let j = 0;
+        for (let i = 0; i < geoms.length; i++) {
+            const cubeMaterial = new THREE.MeshLambertMaterial({
+                wireframe: true,
+                color: Math.random() * 0xffffff
+            });
+
+            const materials = [
+
+                new THREE.MeshLambertMaterial({
+                    color: Math.random() * 0xffffff
+                }),
+                new THREE.MeshBasicMaterial({
+                    color: 0x000000,
+                    wireframe: true
+                })
+
+            ];
+
+            const mesh = SceneUtils.createMultiMaterialObject(geoms[i], materials);
+            mesh.traverse(function (e) {
+                e.castShadow = true
+            });
+
+            //var mesh = new THREE.Mesh(geoms[i],materials[i]);
+            //mesh.castShadow=true;
+            mesh.position.x = -24 + ((i % 4) * 12);
+            mesh.position.y = 4;
+            mesh.position.z = -8 + (j * 12);
+
+            if ((i + 1) % 4 == 0) j++;
+            scene.add(mesh);
+        }
+    };
+
+    addGeometries(scene);
     document.body.appendChild(renderer.domElement);
+
 
     const stats = new Stats();
     stats.showPanel(0);
@@ -52,15 +134,14 @@ const init = () => {
 
     document.body.appendChild(stats.dom);
 
-    const controlMove = new TrackballControls(camera, renderer.domElement);
-    controlMove.rotateSpeed = 2;
-    controlMove.dynamicDampingFactor = 0.15;
+    const trackballControls = new TrackballControls(camera, renderer.domElement);
+    const clock = new THREE.Clock();
 
-    addGeometries(scene, renderer);
 
     const render = () => {
         stats.update();
-        controlMove.update();
+        // controlMove.update();
+        trackballControls.update(clock.getDelta());
 
         requestAnimationFrame(render);
         renderer.render(scene, camera);
@@ -68,61 +149,3 @@ const init = () => {
     render();
 };
 window.onload = init;
-
-const addGeometries = (scene, renderer) => {
-    const geoms = [];
-
-    geoms.push(new THREE.CylinderGeometry(1, 4, 4));
-    geoms.push(new THREE.BoxGeometry(2, 2, 2));
-    geoms.push(new THREE.SphereGeometry(2));
-    geoms.push(new THREE.IcosahedronGeometry(4));
-
-    const points = [
-        new THREE.Vector3(2, 2, 2),
-        new THREE.Vector3(2, 2, -2),
-        new THREE.Vector3(-2, 2, -2),
-        new THREE.Vector3(-2, 2, 2),
-        new THREE.Vector3(2, -2, 2),
-        new THREE.Vector3(2, -2, -2),
-        new THREE.Vector3(-2, -2, -2),
-        new THREE.Vector3(-2, -2, 2)
-    ];
-    // geoms.push(new THREE.ConvexGeometry(points));
-
-    const pts = [];
-    const detail = 0.1;
-    const radius = 3;
-    for (let angle = 0.0; angle < Math.PI; angle += detail)
-        pts.push(new THREE.Vector3(Math.cos(angle) * radius, 0, Math.sin(angle) * radius));
-    geoms.push(new THREE.LatheGeometry(pts, 12));
-
-    geoms.push(new THREE.OctahedronGeometry(3));
-
-    // geoms.push(new THREE.ParametricGeometry(THREE.ParametricGeometries.mobius3d, 20, 10));
-
-    geoms.push(new THREE.TetrahedronGeometry(3));
-
-    geoms.push(new THREE.TorusGeometry(3, 1, 10, 10));
-
-    geoms.push(new THREE.TorusKnotGeometry(3, 0.5, 50, 20));
-
-    let j = 0;
-    for (let i = 0; i < geoms.length; i++) {
-        const cubeMaterial = new THREE.MeshLambertMaterial({ wireframe: true, color: Math.random() * 0xffffff });
-
-        const materials = [
-            new THREE.MeshLambertMaterial({ color: Math.random() * 0xffffff, shading: THREE.FlatShading }),
-            new THREE.MeshBasicMaterial({ color: 0x000000, wireframe: true })
-        ];
-
-        const mesh = new THREE.Mesh(geoms[i], materials);
-        mesh.castShadow = true;
-
-        mesh.position.x = -24 + ((i % 4) * 12);
-        mesh.position.y = 4;
-        mesh.position.z = -8 + (j * 12);
-
-        if ((i + 1) % 4 == 0) j++;
-        scene.add(mesh);
-    }
-};
